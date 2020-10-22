51467265e7bed5a1b9d508c46e3b121281f76c2a
gayasuddin <gayasuddin.siddiqui@mountblue.tech>
2020-10-22 15:17:06 +0530

    Get all products

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,7 +1,43 @@
 const { product, product_metaData, media } = require("../models/product");
 
 // Get all products
-exports.getProducts = async (req, res) => {};
+exports.getProducts = async (req, res) => {
+  const products = await product
+    .findAll({
+      include: [
+        {
+          model: media,
+        },
+      ],
+    })
+    .then((products) => {
+      return products.map((product) => {
+        //tidy up the product data
+        return Object.assign(
+          {},
+          {
+            product_id: product.id,
+            name: product.name,
+            price: product.price,
+            media: product.media.map((image) => {
+              return Object.assign(
+                {},
+                {
+                  url: image.url,
+                  resource_id: image.resource_id,
+                }
+              );
+            }),
+          }
+        );
+      });
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((err) => console.error(err.message));
+};
 
 // Get product by id
 exports.getProducts = async (req, res) => {
