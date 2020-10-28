ce5562d017b71b7f67fd791c752153ce28f16e59
gayasuddin <gayasuddin.siddiqui@mountblue.tech>
2020-10-28 10:58:18 +0530

    Uncommented cors

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-// const cors = require("cors");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -25,7 +25,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-// app.use(cors());
+app.use(cors());
 app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", userRoutes);
